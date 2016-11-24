69cea8d58df7755ab83c4b54d0d906ce6bf887cc
Khaled Halabieh <khaled-h@hotmail.it>
2016-11-24 21:40:07 +0100

    Update IdentityMatrix.java

--- uebung6/IdentityMatrix.java
+++ uebung6/IdentityMatrix.java
@@ -4,8 +4,12 @@ public class IdentityMatrix extends QuadraticMatrix {
 	public IdentityMatrix(int m) {
 		super(m);
 		for (int i = 0; i < m; i++) {
+			komponenten[i][i] = 0.0f;
+		}
+			for (i = 0; i < komponenten.length; i++) {
 			komponenten[i][i] = 1.0f;
 		}
+		
 	}
 
 	public String toString() {
